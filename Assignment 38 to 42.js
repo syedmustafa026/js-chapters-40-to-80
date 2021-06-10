779d7cb126f20948bfdca0b73ad4bde251597fae
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-06-10 23:24:36 +0500

    complete

--- Assignment 38 to 42.js
+++ Assignment 38 to 42.js
@@ -29,15 +29,83 @@
 // }
 // leap(d)
 
-//                                 QUESTION 3
-// var a = prompt("Enter first Number")
-// var b = prompt("Enter sec Number")
-// var c = prompt("Enter third Number")
-// function areas(a,b,c){
-// var S = ( a + b + c ) / 2;
-// area = S *(S - a)*(S - b)*(S - c);
 
-// console.log("The area is "+area)
+//                                            Question 3
 
+// var a = 5; 
+// var b = 6; 
+// var c = 7; 
+// var s;
+// function two(a,b,c,s){
+//     function one(a,b,c){
+//         var s=(a + b + c)/2;
+//         return s;
+//     }
+//     s = one(a,b,c);
+//     console.log(s);
+
+//     var area= (s*((s-a)*(s-b)*(s-c)));
+//     return area;
 // }
-// areas(a,b,c)
+// var area= two(a,b,c,s);
+// console.log(area);
+
+//                                            Question 4
+
+// function main(){
+//     var english=+prompt("Enter English marks");
+//     var maths=+prompt("Enter Maths marks");
+//     var computer=+prompt("Enter Computer marks");
+//     var avg;
+//     var per;
+//     function average(a,b,c){
+//         var aver =a+b+c;
+//         return aver;
+//     }
+//     avg =average(english,maths,computer)
+//     console.log(avg);
+//     function percentage(a){
+//      var  percent=(a/300)*100;
+//      return percent;
+//     }
+//     per= percentage(avg);
+//     console.log(per);
+// }
+// main();
+
+//                                            Question 5
+
+// function check(){
+//         var char=prompt("Enter a character");
+//         var name ="abcdefghijklmnopqrstuvwxyz";
+//         console.log(name.length);
+//         for(var i=0;i<name.length;i++){
+//             var charCheck=name[i];
+//             if(char==charCheck){
+//                 console.log("Index is "+i);
+//                 break;
+//             }
+//            else{
+//             "invalid  input";
+//             }
+//         }
+        
+//     }
+//     check();
+
+//                                            Question 6
+
+// function vowel(str) {             
+//     var newString = " ";                            
+//     for (let i = 0; i < str.length; i++)  
+// {           
+//       if (str[i] != "a" && str[i] != "e" && str[i] != "i" && str[i] != "o" && str[i] != "u")                                   
+//        {           
+//        newString += str[i];         
+//       };   
+//     };   
+//     return newString;   
+//   };  
+//   console.log(vowel(prompt("Enter a string to remove vowels")));
+
+
