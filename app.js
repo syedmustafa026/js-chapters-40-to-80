25125430ca928bbbe0e745515e418f5c1e046bc2
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-07-01 01:20:39 +0500

    JS file

---
+++ app.js
@@ -0,0 +1,46 @@
+//                                    OBJECTS
+//                                   QUESTION 1
+// var things = [
+//     { name: "juice", price: "50", quantity: "3" },
+//     { name: "cookie", price: "30", quantity: "9" },
+//     { name: "shirt", price: "880", quantity: "1" },
+//     { name: "pen", price: "100", quantity: "2" }
+// ];
+// console.log(things[0].price + things[1].price + things[2].price)
+// console.log(things[0].price * things[0].quantity + things[1].price * things[1].quantity + things[2].price * things[2].quantity)
+
+//                                   QUESTION 2
+
+// var property = {
+//     name: "Mustafa Ahmed",
+//     email: "mustafa@gmail.com",
+//     password: "12345",
+//     age: "18",
+//     gender: "Male",
+//     city: "Karachi",
+//     country: "Pakistan",
+// }
+
+// console.log("age" in property)
+// console.log("country" in property)
+// console.log("firstName" in property)
+// console.log("lastName" in property)
+
+
+//                                   QUESTION 3
+
+
+// function Student(name,id,gender,password){
+//     this.name = name;
+//     this.id = id;
+//     this.gender = gender;
+//     this.password = password;
+// }
+
+// var plan1 = new Student("Mustafa","224","Male","12345")
+// var plan2 = new Student("Ahmed","22445","Male","54321")
+
+// console.log(plan1)
+// console.log(plan2)
+
+
